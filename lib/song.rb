f7b3875d7647940e6b8be918744186eaeb485fff
candaceww <candacew224@gmail.com>
2018-07-30 16:27:07 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -9,31 +9,6 @@ class Song
     @genre = genre
   
  
- 
-  #   @name = name
-  #   @@count= 0
-  #   @@genres = []
-  #   @@artists = []
-  # end
-  # genres_hash = {}
-  # genres_hash[:genres]
-  
-  # def Song.genres
-  #   puts @@genres
-  # end
-  
-  # def name
-  #   @name
-  # end
-  
-  # def Song.artists
-  #   puts @@artists
-  # end
-  
-  # def Song.genre_count
-  #   puts genres_hash
-  # end
-end
 end
 song_one = Song.new("99 Problems", "Jay-Z", "rap")
 
