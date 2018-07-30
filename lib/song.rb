c50395cda6d4e4c65a55db06aac75878c5cb0208
candaceww <candacew224@gmail.com>
2018-07-30 16:32:17 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,14 +1,13 @@
 class Song
  attr_accessor :name, :artist, :genre
-    count= 0
     genres_hash = {}
     genres_hash[:genre]
-  def initialize(name, artist, genre)
+  def initialize(name, artist, genre, count)
     @name = name
     @artist = artist
     @genre = genre
-    @@count = 0
-  
+    @@count = count
+  count = 0
  
 end
 end
